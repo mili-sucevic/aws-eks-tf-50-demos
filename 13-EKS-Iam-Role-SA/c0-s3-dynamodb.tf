613eaf5cabd51cbd00a640a26a12450c2927ce20
Milica Sucevic <milicasucevic@gmail.com>
2023-01-26 10:27:01 +0000

    update changes of the s3 bucket name

---
+++ 13-EKS-Iam-Role-SA/c0-s3-dynamodb.tf
@@ -0,0 +1,44 @@
+resource "aws_s3_bucket" "dev-bucket" {
+  bucket = "ms-terraform-on-aws-eks"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub1" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "eks-cluster/"
+  content = ""
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub2" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "app1-k8s/"
+  content = ""
+}
+
+
+resource "aws_dynamodb_table" "dev-eks-cluster" {
+  name           = "dev-eks-cluster"
+  read_capacity  = 5
+  write_capacity = 5
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "dev-app1-k8s" {
+  name           = "dev-app1-k8s"
+  read_capacity  = 5
+  write_capacity = 5
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
