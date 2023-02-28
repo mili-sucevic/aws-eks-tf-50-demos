5ac1567d09f8cab5b5e9d4ef21f1448514f3dd4b
Milica Sucevic <milicasucevic@gmail.com>
2023-02-28 13:29:57 +0000

    add tf manifests for creating eks cluster, provision readonly user with iam roles, groups, k8s cr and crb

---
+++ 24-EKS-ReadOnly-User-using-IAMRoles-Groups-k8s-CR-CRB/01-ekscluster-tf-manifests/c10-03-k8s-clusterrole-clusterrolebinding.tf
@@ -0,0 +1,40 @@
+# Resource: Cluster Role
+resource "kubernetes_cluster_role_v1" eks_readonly_clusterrole {
+  metadata {
+    name = "${local.name}-eks-readonly-clusterrole"
+  }
+
+  rule {
+    api_groups = [""] # These come under core APIs
+    resources  = ["nodes", "namespaces", "pods", "events", "services", "configmaps", "serviceaccounts"] #Uncomment for additional Testing
+    #resources  = ["nodes", "namespaces", "pods", "events", "services", "configmaps", "serviceaccounts"] #Uncomment for additional Testing
+    verbs      = ["get", "list"]    
+  }
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "daemonsets", "statefulsets", "replicasets"]
+    verbs      = ["get", "list"]  
+  }
+  rule {
+    api_groups = ["batch"]
+    resources  = ["jobs"]
+    verbs      = ["get", "list"]   
+  }
+}
+
+# Resource: Cluster Role Binding
+resource "kubernetes_cluster_role_binding_v1" "eks_readonly_clusterrolebinding" {
+  metadata {
+    name = "${local.name}-eks-readonly-clusterrolebinding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role_v1.eks_readonly_clusterrole.metadata.0.name 
+  }
+  subject {
+    kind      = "Group"
+    name      = "eks-readonly-group"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
