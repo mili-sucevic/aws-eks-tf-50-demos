502f5bacfd974a50773d06616b6627e59c51544d
Milica Sucevic <milicasucevic@gmail.com>
2023-03-02 11:35:17 +0000

    create eks cluster, install aws lb controller, deploy 3 apps aand svcs in k8s with an ingress for context path based routing

---
+++ 28-EKS-Ingress-Context-Path-Based-Routing/c0-s3-dynamodb.tf
@@ -0,0 +1,32 @@
+# S3 Buckets
+resource "aws_s3_bucket" "dev-bucket" {
+  bucket = "msa-terraform-on-aws-eks"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub1" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "dev/"
+  content = ""
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub2" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "/dev/eks-cluster/"
+  content = ""
+}
+
+# DynamoDB
+resource "aws_dynamodb_table" "dev-eks-cluster" {
+  name           = "dev-eks-cluster"
+  read_capacity  = 5
+  write_capacity = 5
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
