f6cb4527972d37aa4498032f620932e5340af67e
Milica Sucevic <milicasucevic@gmail.com>
2023-03-04 21:48:59 +0000

    rename files

--- 29-EKS-Ingress-SSL-Redirect/02-LB-controller-install-tf-manifests/c3-01-generic-variables.tf
+++ 29-EKS-Ingress-SSL-Redirect/02-LB-controller-install-tf-manifests/c3-01-generic-variables.tf
@@ -9,11 +9,11 @@ variable "aws_region" {
 variable "environment" {
   description = "Environment Variable used as a prefix"
   type        = string
-  default     = "dev"
+  default     = "stag"
 }
 # Business Division
 variable "business_division" {
   description = "Business Division in the large organization this Infrastructure belongs"
   type        = string
-  default     = "SAP"
+  default     = "devops"
 }
