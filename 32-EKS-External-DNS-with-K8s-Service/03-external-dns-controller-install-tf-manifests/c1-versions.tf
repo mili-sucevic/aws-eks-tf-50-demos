bf4cd16a5532af1eb9ae05cf2af6ddbd6bb56d3f
Milica Sucevic <milicasucevic@gmail.com>
2023-03-05 16:01:12 +0000

    add repo for eks cluster, load balancer and external dns controllers tf manifests, and k8s and tf manifests to create app and svcs with load balancer service

---
+++ 32-EKS-External-DNS-with-K8s-Service/03-external-dns-controller-install-tf-manifests/c1-versions.tf
@@ -0,0 +1,38 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.12"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      #version = "2.5.1"
+      version = "~> 2.5"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.11"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "msw-terraform-on-aws-eks"
+    key    = "dev/aws-external-dns/terraform.tfstate"
+    region = "us-east-1"
+
+    # For State Locking
+    dynamodb_table = "dev-aws-external-dns"
+  }
+}
+
+# Terraform AWS Provider Block
+provider "aws" {
+  region = var.aws_region
+}
+
+# Terraform HTTP Provider Block
+provider "http" {
+  # Configuration options
+}
---
+++ 32-EKS-External-DNS-with-K8s-Service/c0-s3-dynamodb.tf
@@ -0,0 +1,68 @@
+# S3 Buckets
+resource "aws_s3_bucket" "dev-bucket" {
+  bucket = "msw-terraform-on-aws-eks"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub1" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "dev/"
+  content = ""
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub2" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "/dev/eks-cluster/"
+  content = ""
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub3" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "/dev/aws-lbc/"
+  content = ""
+}
+
+resource "aws_s3_bucket_object" "dev-bucket-sub4" {
+  bucket  = aws_s3_bucket.dev-bucket.id
+  key     = "/dev/aws-external-dns/"
+  content = ""
+}
+
+# DynamoDB
+resource "aws_dynamodb_table" "dev-eks-cluster" {
+  name           = "dev-eks-cluster"
+  read_capacity  = 5
+  write_capacity = 5
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "dev-aws-lbc" {
+  name           = "dev-aws-lbc"
+  read_capacity  = 5
+  write_capacity = 5
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "dev-aws-external-dns" {
+  name           = "dev-aws-external-dns"
+  read_capacity  = 5
+  write_capacity = 5
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
