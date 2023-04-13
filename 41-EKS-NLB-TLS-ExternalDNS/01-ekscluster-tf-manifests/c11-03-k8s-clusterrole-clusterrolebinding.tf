5adb678c4ea5413fc0101c77781d3401f21b8b3f
Milica Sucevic <milicasucevic@gmail.com>
2023-04-13 16:03:42 +0000

    update code

---
+++ 41-EKS-NLB-TLS-ExternalDNS/01-ekscluster-tf-manifests/c11-03-k8s-clusterrole-clusterrolebinding.tf
@@ -0,0 +1,40 @@
+# Resource: k8s Cluster Role
+resource "kubernetes_cluster_role_v1" "eks_developer_clusterrole" {
+  metadata {
+    name = "${local.name}-eks-developer-clusterrole"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["nodes", "namespaces", "pods", "events", "services"]
+    #resources  = ["nodes", "namespaces", "pods", "events", "services", "configmaps", "serviceaccounts"] #Uncomment for additional Testing
+    verbs = ["get", "list"]
+  }
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "daemonsets", "statefulsets", "replicasets"]
+    verbs      = ["get", "list"]
+  }
+  rule {
+    api_groups = ["batch"]
+    resources  = ["jobs"]
+    verbs      = ["get", "list"]
+  }
+}
+
+# Resource: k8s Cluster Role Binding
+resource "kubernetes_cluster_role_binding_v1" "eks_developer_clusterrolebinding" {
+  metadata {
+    name = "${local.name}-eks-developer-clusterrolebinding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role_v1.eks_developer_clusterrole.metadata.0.name
+  }
+  subject {
+    kind      = "Group"
+    name      = "eks-developer-group"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
